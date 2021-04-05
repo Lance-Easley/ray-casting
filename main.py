99171e1edfd31b483f985a21f2b7e766420221d1
Lance Easley <leasley21@basecampcodingacademy.org>
2021-04-05 16:20:39 -0500

    Progress on new map

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ win = pygame.display.set_mode((screen_x, screen_y))
 win_rect = pygame.Rect(0, 0, screen_x, screen_y)
 
 invisible = pygame.Surface((screen_x, screen_y))
-invisible_image = pygame.image.load("background.png")
+invisible_image = pygame.image.load("nonvisible_map.png")
 invisible.set_colorkey("#123456")
 
 visible = pygame.Surface((screen_x, screen_y))
-visible_image = pygame.image.load("foreground.png")
+visible_image = pygame.image.load("visible_map.png")
 
 pygame.display.set_caption("Ray Casting")
 
@@ -35,38 +35,10 @@ segments = [
 	{"a":{"x":640,"y":360}, "b":{"x":0,"y":360}},
 	{"a":{"x":0,"y":360}, "b":{"x":0,"y":0}},
 
-	# Polygon #1
-	{"a":{"x":100,"y":150}, "b":{"x":120,"y":50}},
-	{"a":{"x":120,"y":50}, "b":{"x":200,"y":80}},
-	{"a":{"x":200,"y":80}, "b":{"x":140,"y":210}},
-	{"a":{"x":140,"y":210}, "b":{"x":100,"y":150}},
-
-	# Polygon #2
-	{"a":{"x":100,"y":200}, "b":{"x":120,"y":250}},
-	{"a":{"x":120,"y":250}, "b":{"x":60,"y":300}},
-	{"a":{"x":60,"y":300}, "b":{"x":100,"y":200}},
-
-	# Polygon #3
-	{"a":{"x":200,"y":260}, "b":{"x":220,"y":150}},
-	{"a":{"x":220,"y":150}, "b":{"x":300,"y":200}},
-	{"a":{"x":300,"y":200}, "b":{"x":350,"y":320}},
-	{"a":{"x":350,"y":320}, "b":{"x":200,"y":260}},
-
-	# Polygon #4
-	{"a":{"x":340,"y":60}, "b":{"x":360,"y":40}},
-	{"a":{"x":360,"y":40}, "b":{"x":370,"y":70}},
-	{"a":{"x":370,"y":70}, "b":{"x":340,"y":60}},
-
-	# Polygon #5
-	{"a":{"x":450,"y":190}, "b":{"x":560,"y":170}},
-	{"a":{"x":560,"y":170}, "b":{"x":540,"y":270}},
-	{"a":{"x":540,"y":270}, "b":{"x":430,"y":290}},
-	{"a":{"x":430,"y":290}, "b":{"x":450,"y":190}},
-
-	# Polygon #6
-	{"a":{"x":400,"y":95}, "b":{"x":580,"y":50}},
-	{"a":{"x":580,"y":50}, "b":{"x":480,"y":150}},
-	{"a":{"x":480,"y":150}, "b":{"x":400,"y":95}}
+	# Outer walls
+	{"a":{"x":24,"y":24}, "b":{"x":608,"y":24}},
+	{"a":{"x":24,"y":24}, "b":{"x":24,"y":248}},
+	{"a":{"x":24,"y":24}, "b":{"x":24,"y":248}},
 
 ]
 
@@ -118,9 +90,9 @@ def draw(segments):
 	mouse = pygame.mouse.get_pos()
 
 	# Draw segments
-	# color = "#999999"
-	# for seg in segments:
-	# 	pygame.draw.line(canvas, color, (seg["a"]["x"], seg["a"]["y"]), (seg["b"]["x"], seg["b"]["y"]), 1)
+	color = "#999999"
+	for seg in segments:
+		pygame.draw.line(win, color, (seg["a"]["x"], seg["a"]["y"]), (seg["b"]["x"], seg["b"]["y"]), 1)
 
 	uniquePoints = []
 	for seg in segments:
