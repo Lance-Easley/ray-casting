da14d5cede10745b1b69a796278fbe2f27afe0e5
Lance Easley <leasley21@basecampcodingacademy.org>
2021-04-15 20:44:01 -0500

    Major performance enhancements

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ win = pygame.display.set_mode((screen_x, screen_y))
 win_rect = pygame.Rect(0, 0, screen_x, screen_y)
 
 invisible = pygame.Surface((screen_x, screen_y))
-invisible_image = pygame.image.load("nonvisible_map.png")
+invisible_image = pygame.image.load("nonvisible_map.png").convert()
 invisible.set_colorkey("#123456")
 
-visible_image = pygame.image.load("visible_map.png")
+visible_image = pygame.image.load("visible_map.png").convert()
 
 pygame.display.set_caption("Ray Casting")
 
+pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN])
 
 #####################################################################################
 # This is a Python-Pygame translation from https://github.com/ncase/sight-and-light #
@@ -29,10 +30,10 @@ pygame.display.set_caption("Ray Casting")
 segments = [
 
 	# Border
-	{"a":{"x":0,"y":0}, "b":{"x":640,"y":0}},
-	{"a":{"x":640,"y":0}, "b":{"x":640,"y":360}},
-	{"a":{"x":640,"y":360}, "b":{"x":0,"y":360}},
-	{"a":{"x":0,"y":360}, "b":{"x":0,"y":0}},
+	{"a":{"x":0,"y":0}, "b":{"x":screen_x,"y":0}},
+	{"a":{"x":screen_x,"y":0}, "b":{"x":screen_x,"y":screen_y}},
+	{"a":{"x":screen_x,"y":screen_y}, "b":{"x":0,"y":screen_y}},
+	{"a":{"x":0,"y":screen_y}, "b":{"x":0,"y":0}},
 
 	# Outer walls (-x+y corner - counter-clockwise)
 	{"a":{"x":24,"y":24}, "b":{"x":24,"y":248}},
@@ -191,8 +192,20 @@ segments = [
 
 ]
 
+segment_deltas = set()
+for segment in segments:
+	# s_px, s_py, s_dx, s_dy
+	segment_deltas.add((segment['a']['x'], segment['a']['y'], segment['b']['x'] - segment['a']['x'], segment['b']['y'] - segment['a']['y']))
+
+uniquePoints = set()
+for seg in segments:
+	if tuple(seg["a"].items()) not in uniquePoints:
+		uniquePoints.add(tuple(seg["a"].items()))
+	if tuple(seg["b"].items()) not in uniquePoints:
+		uniquePoints.add(tuple(seg["b"].items()))
+
 # Find intersection of RAY & SEGMENT
-def getIntersection(ray, segment):
+def getIntersection(ray, segment_delta):
 	# RAY in parametric: Point + Delta*T1
 	r_px = ray['a']['x']
 	r_py = ray['a']['y']
@@ -200,10 +213,7 @@ def getIntersection(ray, segment):
 	r_dy = ray['b']['y'] - ray['a']['y']
 
 	# SEGMENT in parametric: Point + Delta*T2
-	s_px = segment['a']['x']
-	s_py = segment['a']['y']
-	s_dx = segment['b']['x'] - segment['a']['x']
-	s_dy = segment['b']['y'] - segment['a']['y']
+	s_px, s_py, s_dx, s_dy = segment_delta
 
 	# Are they parallel? If so, no intersect
 	if r_dx * s_dy == r_dy * s_dx:
@@ -211,12 +221,8 @@ def getIntersection(ray, segment):
 		return None
 
 	# SOLVE FOR T1 & T2
-	# r_px+r_dx*T1 = s_px+s_dx*T2 && r_py+r_dy*T1 = s_py+s_dy*T2
-	# ==> T1 = (s_px+s_dx*T2-r_px)/r_dx = (s_py+s_dy*T2-r_py)/r_dy
-	# ==> s_px*r_dy + s_dx*T2*r_dy - r_px*r_dy = s_py*r_dx + s_dy*T2*r_dx - r_py*r_dx
-	# ==> T2 = (r_dx*(s_py-r_py) + r_dy*(r_px-s_px))/(s_dx*r_dy - s_dy*r_dx)
 	T2 = (r_dx * (s_py - r_py) + r_dy * (r_px - s_px)) / (s_dx * r_dy - s_dy * r_dx)
-	if r_dy == 0.0:
+	if r_dy == 0:
 		T1 = (s_px + s_dx * T2 - r_px) / r_dx
 	else:
 		T1 = (s_py + s_dy * T2 - r_py) / r_dy
@@ -236,29 +242,22 @@ def getIntersection(ray, segment):
 
 # DRAWING
 def draw(segments):
-	mouse = pygame.mouse.get_pos()
+	mouse_x, mouse_y = pygame.mouse.get_pos()
 
 	# Draw segments
 	# color = "#999999"
 	# for seg in segments:
 	# 	pygame.draw.line(win, color, (seg["a"]["x"], seg["a"]["y"]), (seg["b"]["x"], seg["b"]["y"]), 1)
 
-	uniquePoints = []
-	for seg in segments:
-		if seg["a"] not in uniquePoints:
-			uniquePoints.append(seg["a"])
-		if seg["b"] not in uniquePoints:
-			uniquePoints.append(seg["b"])
-	
-
 	# Get all angles
-	uniqueAngles = []
+	uniqueAngles = set()
 	for uniquePoint in uniquePoints:
-		angle = math.atan2(uniquePoint['y'] - mouse[1], uniquePoint['x'] - mouse[0])
-		uniquePoint['angle'] = angle
-		uniqueAngles.extend([angle-0.00001, angle+0.00001])
+		points_dict = dict(uniquePoint)
+		angle = math.atan2(points_dict['y'] - mouse_y, points_dict['x'] - mouse_x)
+		points_dict['angle'] = angle
+		uniqueAngles.update([angle-0.00001, angle+0.00001])
 
-	uniqueAngles.sort()
+	uniqueAngles = sorted(uniqueAngles)
 
 	# RAYS IN ALL DIRECTIONS
 	intersects = []
@@ -270,49 +269,27 @@ def draw(segments):
 
 		# Ray from center of screen to mouse
 		ray = {
-			"a":{"x": mouse[0], "y": mouse[1]},
-			"b":{"x": mouse[0] + dx, "y": mouse[1] + dy}
+			"a":{"x": mouse_x, "y": mouse_y},
+			"b":{"x": mouse_x + dx, "y": mouse_y + dy}
 		}
 
 		# Find CLOSEST intersection
 		closestIntersect = None
-		for segment in segments:
-			intersect = getIntersection(ray,segment)
+		for segment_delta in segment_deltas:
+			intersect = getIntersection(ray,segment_delta)
 			if not intersect: continue
 			elif not closestIntersect or intersect["param"] < closestIntersect["param"]:
 				closestIntersect = intersect
 
 		# Intersect angle
 		if not closestIntersect: continue
-
 		# Add to list of intersects
-		if closestIntersect:
-			closestIntersect["angle"] = angle
+		else:
 			intersects.append(closestIntersect)
 
-	# DRAW AS A GIANT POLYGON
-	# ctx.fillStyle = "#dd3838";
-	# ctx.beginPath();
-	# ctx.moveTo(intersects[0].x,intersects[0].y);
-	# for(var i=1;i<intersects.length;i++){
-	# 	var intersect = intersects[i];
-	# 	ctx.lineTo(intersect.x,intersect.y);
-	# }
-	# ctx.fill();
-
 	# DRAW ALL RAYS
-	color = "#dd3838"
-	points = []
-	for intersect in intersects:
+	points = [(intersect['x'], intersect['y']) for intersect in intersects]
 
-		# Draw red laser
-		# pygame.draw.line(canvas, color, mouse, (intersect["x"], intersect["y"]), 1)
-		
-		# Draw red dot
-		# pygame.draw.circle(canvas, color, (intersect['x'], intersect['y']), 4)
-
-		# Draw Polygon Shading
-		points.append((intersect['x'], intersect['y']))
 	# # print("\n", points, "\n")
 	invisible.blit(invisible_image, win_rect)
 	pygame.draw.polygon(invisible, "#123456", points)
@@ -336,11 +313,10 @@ if __name__ == '__main__':
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
-		
-		keys = pygame.key.get_pressed()
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					run = False
 
-		if keys[pygame.K_ESCAPE]:
-			run = False
 
 		redrawGameWindow()
 
